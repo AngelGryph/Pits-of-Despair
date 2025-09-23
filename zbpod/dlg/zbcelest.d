d10497c76fe44233ee29d02daf34124c66b6a27f
Angel <angel@pearlgates.net>
2025-09-23 22:31:00 +0200

    Implemented Scarlet Ninja Tabi and Celeste +4

---
+++ zbpod/dlg/zbcelest.d
@@ -0,0 +1,12 @@
+// Sample "hello world" dialog for conversible weapon Celeste.
+
+BEGIN "zbcelest"
+
+IF "True()"
+BEGIN hello
+  SAY ~Hello, I am the magical throwing dagger Celeste!~
+  IF ~~
+    EXIT
+END
+
+
